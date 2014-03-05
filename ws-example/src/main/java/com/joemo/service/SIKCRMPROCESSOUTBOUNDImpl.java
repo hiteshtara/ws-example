fe51898cb82fc7059b99791e16bf5d96872a70c2
Hitesh <hht179@gmail.com>
2014-03-05 16:53:28 -0500

    gfgfhjdfgfhgf

--- ws-example/src/main/java/com/joemo/service/SIKCRMPROCESSOUTBOUNDImpl.java
+++ ws-example/src/main/java/com/joemo/service/SIKCRMPROCESSOUTBOUNDImpl.java
@@ -14,14 +14,16 @@ import com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACE;
 import com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse;
 import com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.GRANTMESSAGES;
 import com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.RETURN;
+import com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.SPONSOREDPROGRAMSMESSAGES;
+import com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.SPONSORMESSAGES;
 import com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.SPXWALKT;
 import com.sap.document.sap.rfc.functions.ZGRANTMESSAGES;
+import com.sap.document.sap.rfc.functions.ZSPONSORMESSAGES;
+import com.sap.document.sap.rfc.functions.ZSPPROGRAMMESSAGES;
 
 import edu.bu.sap.kcrm.SIKCRMPROCESSOUTBOUND;
 
 public class SIKCRMPROCESSOUTBOUNDImpl implements SIKCRMPROCESSOUTBOUND {
-
-	
 
 	@SOAPBinding(parameterStyle = ParameterStyle.BARE)
 	@WebResult(name = "Z_GM_KCRM_INTERFACE.Response", targetNamespace = "urn:sap-com:document:sap:rfc:functions", partName = "parameters")
@@ -30,20 +32,69 @@ public class SIKCRMPROCESSOUTBOUNDImpl implements SIKCRMPROCESSOUTBOUND {
 			ZGMKCRMINTERFACE parameters) {
 		ZGMKCRMINTERFACEResponse t = new ZGMKCRMINTERFACEResponse();
 		GRANTMESSAGES gmssege = new GRANTMESSAGES();
-		List<ZGRANTMESSAGES> TT= gmssege.getItem();
-		
+		List<ZGRANTMESSAGES> TT = gmssege.getItem();
+
 		t.setGRANTMESSAGES(gmssege);
-		SPXWALKT U= new SPXWALKT();
-		  List<ZFIKCRMSPXWALK> k=U.getItem();
-		List<ZFIKCRMSPXWALK> pp= new ArrayList<ZFIKCRMSPXWALK>();
+		SPXWALKT U = new SPXWALKT();
+		List<ZFIKCRMSPXWALK> k = U.getItem();
+		List<ZFIKCRMSPXWALK> pp = new ArrayList<ZFIKCRMSPXWALK>();
+		ZFIKCRMSPXWALK obj0= new ZFIKCRMSPXWALK();
+		obj0.setFNDUNTDPTPRESUF("22222");
+		obj0.setINTERNALORDER("999999");
+		obj0.setSPONSOREDPROG("abcedef");
+		obj0.setSPPROGRAMNUMBER("6666888");
+		obj0.setTYPE("xxxxx");
+		obj0.setZZREFERENCE("ZZZZZZ");
+		pp.add(obj0);
 		k.addAll(pp);
 		t.setSPXWALKT(U);
-		RETURN R= new RETURN();
-		
+		// add return
+		RETURN R = new RETURN();
+
 		List<BAPIRET2> bap = R.getItem();
-		List<BAPIRET2>kk=new ArrayList<BAPIRET2>();
+		List<BAPIRET2> kk = new ArrayList<BAPIRET2>();
+		BAPIRET2 bp2= new BAPIRET2();
+		bp2.setFIELD("99");
+		bp2.setID("6645636");
+		bp2.setLOGMSGNO("bddjvn");
+		bp2.setMESSAGE("program interfacing");
+		kk.add(bp2);
 		bap.addAll(kk);
 		t.setRETURN(R);
+		// ad sponsor program messages
+		SPONSORMESSAGES sp = new SPONSORMESSAGES();
+		List<ZSPONSORMESSAGES> ll = sp.getItem();
+		List<ZSPONSORMESSAGES> ppp = new ArrayList<ZSPONSORMESSAGES>();
+		ZSPONSORMESSAGES obj1= new ZSPONSORMESSAGES();
+		obj1.setMESSAGE("testing sap Interface");
+		obj1.setSPONSOR("BU");
+		obj1.setTYPE("test");
+		ppp.add(obj1);
+		ll.addAll(ppp);
+		t.setSPONSORMESSAGES(sp);
+		// set sponsored program messages
+		SPONSOREDPROGRAMSMESSAGES op = new SPONSOREDPROGRAMSMESSAGES();
+		List<ZSPPROGRAMMESSAGES> st = op.getItem();
+		List<ZSPPROGRAMMESSAGES> pppk = new ArrayList<ZSPPROGRAMMESSAGES>();
+		ZSPPROGRAMMESSAGES obj5= new ZSPPROGRAMMESSAGES();
+		obj5.setMESSAGE("pppp");
+		obj5.setSPONSOREDPROG("6654");
+		obj5.setTYPE("uuu");
+		pppk.add(obj5);
+		st.addAll(pppk);
+
+		t.setSPONSOREDPROGRAMSMESSAGES(op);
+		// now set grant messages
+		GRANTMESSAGES gm = new GRANTMESSAGES();
+		List<ZGRANTMESSAGES> SGM = gm.getItem();
+		List<ZGRANTMESSAGES> SGMM = new ArrayList<ZGRANTMESSAGES>();
+		ZGRANTMESSAGES obj2= new ZGRANTMESSAGES();
+		obj2.setGRANTNBR("1234");
+		obj2.setMESSAGE("testing sap award");
+		obj2.setTYPE("test");
+		SGM.addAll(SGMM);
+		t.setGRANTMESSAGES(gm);
+
 		return t;
 	}
 
--- ws-example/target/generated/src/main/java/com/sap/document/sap/rfc/functions/ObjectFactory.java
+++ ws-example/target/generated/src/main/java/com/sap/document/sap/rfc/functions/ObjectFactory.java
@@ -38,155 +38,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link ZGMSPPROGRAMSTRUCTURE }
-     * 
-     */
-    public ZGMSPPROGRAMSTRUCTURE createZGMSPPROGRAMSTRUCTURE() {
-        return new ZGMSPPROGRAMSTRUCTURE();
-    }
-
-    /**
-     * Create an instance of {@link GRANTMESSAGES }
-     * 
-     */
-    public GRANTMESSAGES createZGMKCRMINTERFACEResponseGRANTMESSAGES() {
-        return new GRANTMESSAGES();
-    }
-
-    /**
-     * Create an instance of {@link ZSPPROGRAMMESSAGES }
-     * 
-     */
-    public ZSPPROGRAMMESSAGES createZSPPROGRAMMESSAGES() {
-        return new ZSPPROGRAMMESSAGES();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035HEADERADD }
-     * 
-     */
-    public ZBAPI0035HEADERADD createZBAPI0035HEADERADD() {
-        return new ZBAPI0035HEADERADD();
-    }
-
-    /**
-     * Create an instance of {@link ZGMSPRESPONSIBLEKCRM }
-     * 
-     */
-    public ZGMSPRESPONSIBLEKCRM createZGMSPRESPONSIBLEKCRM() {
-        return new ZGMSPRESPONSIBLEKCRM();
-    }
-
-    /**
-     * Create an instance of {@link GMSPPROGRAMFMBTTT }
-     * 
-     */
-    public GMSPPROGRAMFMBTTT createGMSPPROGRAMFMBTTT() {
-        return new GMSPPROGRAMFMBTTT();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035BUDGETVALIDITYT }
-     * 
-     */
-    public ZBAPI0035BUDGETVALIDITYT createZBAPI0035BUDGETVALIDITYT() {
-        return new ZBAPI0035BUDGETVALIDITYT();
-    }
-
-    /**
-     * Create an instance of {@link ZGMFACREDITT }
-     * 
-     */
-    public ZGMFACREDITT createZGMFACREDITT() {
-        return new ZGMFACREDITT();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035HEADER }
-     * 
-     */
-    public ZBAPI0035HEADER createZBAPI0035HEADER() {
-        return new ZBAPI0035HEADER();
-    }
-
-    /**
-     * Create an instance of {@link ZGMKCRMINTERFACE }
-     * 
-     */
-    public ZGMKCRMINTERFACE createZGMKCRMINTERFACE() {
-        return new ZGMKCRMINTERFACE();
-    }
-
-    /**
-     * Create an instance of {@link ZGMKCRMINTERFACEResponse }
-     * 
-     */
-    public ZGMKCRMINTERFACEResponse createZGMKCRMINTERFACEResponse() {
-        return new ZGMKCRMINTERFACEResponse();
-    }
-
-    /**
      * Create an instance of {@link SPONSOREDPROGRAMSMESSAGES }
      * 
      */
     public SPONSOREDPROGRAMSMESSAGES createZGMKCRMINTERFACEResponseSPONSOREDPROGRAMSMESSAGES() {
         return new SPONSOREDPROGRAMSMESSAGES();
-    }
-
-    /**
-     * Create an instance of {@link SPONSOR }
-     * 
-     */
-    public SPONSOR createZGMKCRMINTERFACESPONSOR() {
-        return new SPONSOR();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035RESPONSIBLET }
-     * 
-     */
-    public ZBAPI0035RESPONSIBLET createZBAPI0035RESPONSIBLET() {
-        return new ZBAPI0035RESPONSIBLET();
-    }
-
-    /**
-     * Create an instance of {@link ZFIGRANTDATA }
-     * 
-     */
-    public ZFIGRANTDATA createZFIGRANTDATA() {
-        return new ZFIGRANTDATA();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035SPONSOREDPROGT }
-     * 
-     */
-    public ZBAPI0035SPONSOREDPROGT createZBAPI0035SPONSOREDPROGT() {
-        return new ZBAPI0035SPONSOREDPROGT();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035SPONSOREDOBJECTS }
-     * 
-     */
-    public ZBAPI0035SPONSOREDOBJECTS createZBAPI0035SPONSOREDOBJECTS() {
-        return new ZBAPI0035SPONSOREDOBJECTS();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035FUNDS }
-     * 
-     */
-    public ZBAPI0035FUNDS createZBAPI0035FUNDS() {
-        return new ZBAPI0035FUNDS();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035GRANTDESCRIPTION }
-     * 
-     */
-    public ZBAPI0035GRANTDESCRIPTION createZBAPI0035GRANTDESCRIPTION() {
-        return new ZBAPI0035GRANTDESCRIPTION();
     }
 
     /**
@@ -198,139 +54,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link ZGRANTMESSAGES }
+     * Create an instance of {@link com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.RETURN }
      * 
      */
-    public ZGRANTMESSAGES createZGRANTMESSAGES() {
-        return new ZGRANTMESSAGES();
-    }
-
-    /**
-     * Create an instance of {@link BILLINGPLAN }
-     * 
-     */
-    public BILLINGPLAN createZGMKCRMINTERFACEBILLINGPLAN() {
-        return new BILLINGPLAN();
-    }
-
-    /**
-     * Create an instance of {@link ZGMSPPROGRAMGRPSTRUCTURE }
-     * 
-     */
-    public ZGMSPPROGRAMGRPSTRUCTURE createZGMSPPROGRAMGRPSTRUCTURE() {
-        return new ZGMSPPROGRAMGRPSTRUCTURE();
-    }
-
-    /**
-     * Create an instance of {@link GMSPPROGRAMFMBT }
-     * 
-     */
-    public GMSPPROGRAMFMBT createGMSPPROGRAMFMBT() {
-        return new GMSPPROGRAMFMBT();
-    }
-
-    /**
-     * Create an instance of {@link com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACE.RETURN }
-     * 
-     */
-    public com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACE.RETURN createZGMKCRMINTERFACERETURN() {
-        return new com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACE.RETURN();
-    }
-
-    /**
-     * Create an instance of {@link SPXWALKT }
-     * 
-     */
-    public SPXWALKT createZGMKCRMINTERFACEResponseSPXWALKT() {
-        return new SPXWALKT();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035BUDGETVALIDITY }
-     * 
-     */
-    public ZBAPI0035BUDGETVALIDITY createZBAPI0035BUDGETVALIDITY() {
-        return new ZBAPI0035BUDGETVALIDITY();
-    }
-
-    /**
-     * Create an instance of {@link ZSPONSORMESSAGES }
-     * 
-     */
-    public ZSPONSORMESSAGES createZSPONSORMESSAGES() {
-        return new ZSPONSORMESSAGES();
-    }
-
-    /**
-     * Create an instance of {@link ZGMGRANTSTRUCTURE }
-     * 
-     */
-    public ZGMGRANTSTRUCTURE createZGMGRANTSTRUCTURE() {
-        return new ZGMGRANTSTRUCTURE();
-    }
-
-    /**
-     * Create an instance of {@link ZGMBILLINGPLANSTRUCTURE }
-     * 
-     */
-    public ZGMBILLINGPLANSTRUCTURE createZGMBILLINGPLANSTRUCTURE() {
-        return new ZGMBILLINGPLANSTRUCTURE();
-    }
-
-    /**
-     * Create an instance of {@link BAPIRET2 }
-     * 
-     */
-    public BAPIRET2 createBAPIRET2() {
-        return new BAPIRET2();
-    }
-
-    /**
-     * Create an instance of {@link SPONSORMESSAGES }
-     * 
-     */
-    public SPONSORMESSAGES createZGMKCRMINTERFACEResponseSPONSORMESSAGES() {
-        return new SPONSORMESSAGES();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035SPONSOREDOBJECTST }
-     * 
-     */
-    public ZBAPI0035SPONSOREDOBJECTST createZBAPI0035SPONSOREDOBJECTST() {
-        return new ZBAPI0035SPONSOREDOBJECTST();
-    }
-
-    /**
-     * Create an instance of {@link SPONSOREDPROGRAMSGRP }
-     * 
-     */
-    public SPONSOREDPROGRAMSGRP createZGMKCRMINTERFACESPONSOREDPROGRAMSGRP() {
-        return new SPONSOREDPROGRAMSGRP();
-    }
-
-    /**
-     * Create an instance of {@link ZBAPI0035RESPONSIBLE }
-     * 
-     */
-    public ZBAPI0035RESPONSIBLE createZBAPI0035RESPONSIBLE() {
-        return new ZBAPI0035RESPONSIBLE();
-    }
-
-    /**
-     * Create an instance of {@link ZFIKCRMSPXWALK }
-     * 
-     */
-    public ZFIKCRMSPXWALK createZFIKCRMSPXWALK() {
-        return new ZFIKCRMSPXWALK();
-    }
-
-    /**
-     * Create an instance of {@link ZGMFACREDIT }
-     * 
-     */
-    public ZGMFACREDIT createZGMFACREDIT() {
-        return new ZGMFACREDIT();
+    public com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.RETURN createZGMKCRMINTERFACEResponseRETURN() {
+        return new com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.RETURN();
     }
 
     /**
@@ -342,11 +70,171 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link ZFIGMSPRESPONSIBLETABKCRM }
+     * Create an instance of {@link SPXWALKT }
      * 
      */
-    public ZFIGMSPRESPONSIBLETABKCRM createZFIGMSPRESPONSIBLETABKCRM() {
-        return new ZFIGMSPRESPONSIBLETABKCRM();
+    public SPXWALKT createZGMKCRMINTERFACEResponseSPXWALKT() {
+        return new SPXWALKT();
+    }
+
+    /**
+     * Create an instance of {@link ZGMSPPROGRAMGRPSTRUCTURE }
+     * 
+     */
+    public ZGMSPPROGRAMGRPSTRUCTURE createZGMSPPROGRAMGRPSTRUCTURE() {
+        return new ZGMSPPROGRAMGRPSTRUCTURE();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035HEADERADD }
+     * 
+     */
+    public ZBAPI0035HEADERADD createZBAPI0035HEADERADD() {
+        return new ZBAPI0035HEADERADD();
+    }
+
+    /**
+     * Create an instance of {@link SPONSOREDPROGRAMSGRP }
+     * 
+     */
+    public SPONSOREDPROGRAMSGRP createZGMKCRMINTERFACESPONSOREDPROGRAMSGRP() {
+        return new SPONSOREDPROGRAMSGRP();
+    }
+
+    /**
+     * Create an instance of {@link com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACE.RETURN }
+     * 
+     */
+    public com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACE.RETURN createZGMKCRMINTERFACERETURN() {
+        return new com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACE.RETURN();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035FUNDS }
+     * 
+     */
+    public ZBAPI0035FUNDS createZBAPI0035FUNDS() {
+        return new ZBAPI0035FUNDS();
+    }
+
+    /**
+     * Create an instance of {@link ZFIKCRMSPXWALK }
+     * 
+     */
+    public ZFIKCRMSPXWALK createZFIKCRMSPXWALK() {
+        return new ZFIKCRMSPXWALK();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035BUDGETVALIDITYT }
+     * 
+     */
+    public ZBAPI0035BUDGETVALIDITYT createZBAPI0035BUDGETVALIDITYT() {
+        return new ZBAPI0035BUDGETVALIDITYT();
+    }
+
+    /**
+     * Create an instance of {@link ZGMGRANTSTRUCTURE }
+     * 
+     */
+    public ZGMGRANTSTRUCTURE createZGMGRANTSTRUCTURE() {
+        return new ZGMGRANTSTRUCTURE();
+    }
+
+    /**
+     * Create an instance of {@link SPONSORMESSAGES }
+     * 
+     */
+    public SPONSORMESSAGES createZGMKCRMINTERFACEResponseSPONSORMESSAGES() {
+        return new SPONSORMESSAGES();
+    }
+
+    /**
+     * Create an instance of {@link ZGRANTMESSAGES }
+     * 
+     */
+    public ZGRANTMESSAGES createZGRANTMESSAGES() {
+        return new ZGRANTMESSAGES();
+    }
+
+    /**
+     * Create an instance of {@link ZSPPROGRAMMESSAGES }
+     * 
+     */
+    public ZSPPROGRAMMESSAGES createZSPPROGRAMMESSAGES() {
+        return new ZSPPROGRAMMESSAGES();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035HEADER }
+     * 
+     */
+    public ZBAPI0035HEADER createZBAPI0035HEADER() {
+        return new ZBAPI0035HEADER();
+    }
+
+    /**
+     * Create an instance of {@link GRANTMESSAGES }
+     * 
+     */
+    public GRANTMESSAGES createZGMKCRMINTERFACEResponseGRANTMESSAGES() {
+        return new GRANTMESSAGES();
+    }
+
+    /**
+     * Create an instance of {@link BAPIRET2 }
+     * 
+     */
+    public BAPIRET2 createBAPIRET2() {
+        return new BAPIRET2();
+    }
+
+    /**
+     * Create an instance of {@link BILLINGPLAN }
+     * 
+     */
+    public BILLINGPLAN createZGMKCRMINTERFACEBILLINGPLAN() {
+        return new BILLINGPLAN();
+    }
+
+    /**
+     * Create an instance of {@link ZGMBILLINGPLANSTRUCTURE }
+     * 
+     */
+    public ZGMBILLINGPLANSTRUCTURE createZGMBILLINGPLANSTRUCTURE() {
+        return new ZGMBILLINGPLANSTRUCTURE();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035SPONSOREDOBJECTST }
+     * 
+     */
+    public ZBAPI0035SPONSOREDOBJECTST createZBAPI0035SPONSOREDOBJECTST() {
+        return new ZBAPI0035SPONSOREDOBJECTST();
+    }
+
+    /**
+     * Create an instance of {@link ZGMSPRESPONSIBLEKCRM }
+     * 
+     */
+    public ZGMSPRESPONSIBLEKCRM createZGMSPRESPONSIBLEKCRM() {
+        return new ZGMSPRESPONSIBLEKCRM();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035SPONSOREDOBJECTS }
+     * 
+     */
+    public ZBAPI0035SPONSOREDOBJECTS createZBAPI0035SPONSOREDOBJECTS() {
+        return new ZBAPI0035SPONSOREDOBJECTS();
+    }
+
+    /**
+     * Create an instance of {@link ZGMFACREDIT }
+     * 
+     */
+    public ZGMFACREDIT createZGMFACREDIT() {
+        return new ZGMFACREDIT();
     }
 
     /**
@@ -358,11 +246,19 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.RETURN }
+     * Create an instance of {@link GMSPPROGRAMFMBTTT }
      * 
      */
-    public com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.RETURN createZGMKCRMINTERFACEResponseRETURN() {
-        return new com.sap.document.sap.rfc.functions.ZGMKCRMINTERFACEResponse.RETURN();
+    public GMSPPROGRAMFMBTTT createGMSPPROGRAMFMBTTT() {
+        return new GMSPPROGRAMFMBTTT();
+    }
+
+    /**
+     * Create an instance of {@link ZFIGRANTDATA }
+     * 
+     */
+    public ZFIGRANTDATA createZFIGRANTDATA() {
+        return new ZFIGRANTDATA();
     }
 
     /**
@@ -371,6 +267,110 @@ public class ObjectFactory {
      */
     public ZBAPI0035SPONSOREDPROG createZBAPI0035SPONSOREDPROG() {
         return new ZBAPI0035SPONSOREDPROG();
+    }
+
+    /**
+     * Create an instance of {@link ZGMFACREDITT }
+     * 
+     */
+    public ZGMFACREDITT createZGMFACREDITT() {
+        return new ZGMFACREDITT();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035RESPONSIBLE }
+     * 
+     */
+    public ZBAPI0035RESPONSIBLE createZBAPI0035RESPONSIBLE() {
+        return new ZBAPI0035RESPONSIBLE();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035SPONSOREDPROGT }
+     * 
+     */
+    public ZBAPI0035SPONSOREDPROGT createZBAPI0035SPONSOREDPROGT() {
+        return new ZBAPI0035SPONSOREDPROGT();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035GRANTDESCRIPTION }
+     * 
+     */
+    public ZBAPI0035GRANTDESCRIPTION createZBAPI0035GRANTDESCRIPTION() {
+        return new ZBAPI0035GRANTDESCRIPTION();
+    }
+
+    /**
+     * Create an instance of {@link ZGMKCRMINTERFACE }
+     * 
+     */
+    public ZGMKCRMINTERFACE createZGMKCRMINTERFACE() {
+        return new ZGMKCRMINTERFACE();
+    }
+
+    /**
+     * Create an instance of {@link ZFIGMSPRESPONSIBLETABKCRM }
+     * 
+     */
+    public ZFIGMSPRESPONSIBLETABKCRM createZFIGMSPRESPONSIBLETABKCRM() {
+        return new ZFIGMSPRESPONSIBLETABKCRM();
+    }
+
+    /**
+     * Create an instance of {@link ZGMKCRMINTERFACEResponse }
+     * 
+     */
+    public ZGMKCRMINTERFACEResponse createZGMKCRMINTERFACEResponse() {
+        return new ZGMKCRMINTERFACEResponse();
+    }
+
+    /**
+     * Create an instance of {@link SPONSOR }
+     * 
+     */
+    public SPONSOR createZGMKCRMINTERFACESPONSOR() {
+        return new SPONSOR();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035BUDGETVALIDITY }
+     * 
+     */
+    public ZBAPI0035BUDGETVALIDITY createZBAPI0035BUDGETVALIDITY() {
+        return new ZBAPI0035BUDGETVALIDITY();
+    }
+
+    /**
+     * Create an instance of {@link ZGMSPPROGRAMSTRUCTURE }
+     * 
+     */
+    public ZGMSPPROGRAMSTRUCTURE createZGMSPPROGRAMSTRUCTURE() {
+        return new ZGMSPPROGRAMSTRUCTURE();
+    }
+
+    /**
+     * Create an instance of {@link ZBAPI0035RESPONSIBLET }
+     * 
+     */
+    public ZBAPI0035RESPONSIBLET createZBAPI0035RESPONSIBLET() {
+        return new ZBAPI0035RESPONSIBLET();
+    }
+
+    /**
+     * Create an instance of {@link GMSPPROGRAMFMBT }
+     * 
+     */
+    public GMSPPROGRAMFMBT createGMSPPROGRAMFMBT() {
+        return new GMSPPROGRAMFMBT();
+    }
+
+    /**
+     * Create an instance of {@link ZSPONSORMESSAGES }
+     * 
+     */
+    public ZSPONSORMESSAGES createZSPONSORMESSAGES() {
+        return new ZSPONSORMESSAGES();
     }
 
 }
